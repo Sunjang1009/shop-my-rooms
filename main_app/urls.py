53e6138d688e9a32a2beb6998d95f0af89a98eed
Sunjang1009 <sunjang1009@gmail.com>
2023-07-08 16:03:47 -0500

    setting up themes url, view, mock database

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('themes/', views.ThemeList.as_view(), name="theme_list"),
 ]
+
+
 
 
