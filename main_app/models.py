f9aa90c7e40c2dd38125b7d269b2a2453adafb85
Sunjang1009 <sunjang1009@gmail.com>
2023-07-10 18:08:09 -0500

    added admin and updated database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Theme(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    description = models.TextField(max_length=500)
+    def __str__(self):
+        return self.name
+    
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Theme
+
 # Create your views here.
 
 class Home(TemplateView):
@@ -10,22 +12,17 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-class Theme:
-    def __init__(self, name, image):
-        self.name = name
-        self.image = image
-
-themes = [
-    Theme("modern", "https://i.pinimg.com/236x/09/71/17/09711788c0cda08d318e1b446e739003.jpg"),
-    Theme("colorful", "https://i.pinimg.com/236x/da/6f/f3/da6ff36deea78497b5d3cfb1ba8c8355.jpg"),
-    Theme("bohemian", "https://i.pinimg.com/236x/0d/cb/14/0dcb14d1029cdc29d51d23aed35d2ac1.jpg"),
-    Theme("romantic", "https://i.pinimg.com/564x/92/6c/0e/926c0ecad7c6afd7149902697b4eea10.jpg"),
-]
+# themes = [
+#     Theme("modern", "https://i.pinimg.com/236x/09/71/17/09711788c0cda08d318e1b446e739003.jpg"),
+#     Theme("colorful", "https://i.pinimg.com/236x/da/6f/f3/da6ff36deea78497b5d3cfb1ba8c8355.jpg"),
+#     Theme("bohemian", "https://i.pinimg.com/236x/0d/cb/14/0dcb14d1029cdc29d51d23aed35d2ac1.jpg"),
+#     Theme("romantic", "https://i.pinimg.com/564x/92/6c/0e/926c0ecad7c6afd7149902697b4eea10.jpg"),
+# ]
 
 class ThemeList(TemplateView):
     template_name = "theme_list.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["themes"] = themes
+        context["themes"] = Theme.objects.all()
         return context
 
