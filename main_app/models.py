7bb6b225be67493a5375c50ce576a7b4b505ef1e
Sunjang1009 <sunjang1009@gmail.com>
2023-07-10 21:16:01 -0500

    added Product model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
 class Theme(models.Model):
@@ -16,4 +17,15 @@ class Lookbook(models.Model):
     def __str__(self):
         return self.name
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('100.00'))
+    lookbook = models.ForeignKey(Lookbook, on_delete=models.CASCADE, related_name="products")
+    def __str__(self):
+        return self.name
+
+    def get_price(self):
+        return f"${self.price:,.2f}"
 
